46d9aa83654e4f61915c850fba437e4b5393db6c
webhecostaucl <80169813+webhecostaucl@users.noreply.github.com>
2021-09-15 23:24:55 +0000

    Final

--- Veiculo.cs
+++ Veiculo.cs
@@ -3,43 +3,64 @@ using System.Collections.Generic;
 
 class Veiculo {
 
-  public string cor {get ; protected set;}
-  public string marca{get ; protected set;}
-  public string modelo{get ; protected set;}
-  public double valor{get ; protected set;}
-  public string anoFabri{get ; protected set;}
-  public double custo_Fixo{get ; protected set;}
+  public string cor {
+    get;
+    protected set;
+  }
+  public string marca {
+    get;
+    protected set;
+  }
+  public string modelo {
+    get;
+    protected set;
+  }
+  public double valor {
+    get;
+    protected set;
+  }
+  public string anoFabri {
+    get;
+    protected set;
+  }
+  public double custo_Fixo {
+    get;
+    protected set;
+  }
+
+  public Veiculo(){
+    
+  }
 
   //Construtor Completo
-  public Veiculo (string cr,string ma,string mod,double vl, string aF, double cF ){
+  public Veiculo(string co, string ma, string mod, double vl, string aF, double cF) {
 
-    cor = cr;
+    cor = co;
     marca = ma;
     modelo = mod;
     valor = vl;
     anoFabri = aF;
     custo_Fixo = cF;
   }
-  
-  protected virtual void definirValorVeiculo (double  CustoFixo){
 
-    this.valor = CustoFixo;
+  protected virtual void definirValorVeiculo(double CustoFixo) {
+
+    valor = CustoFixo;
 
   }
 
   //Mudar atributos
-  public void mudarAtributos(string cr, string vl,double c){
-    cor = cr;
+  public void mudarAtributos(string co, double vl, double c) {
+    cor = co;
     valor = vl;
     custo_Fixo = c;
   }
 
-   public void mudarAtributos(string cr, string vl,double c,string ano){
-    cor = cr;
+  public void mudarAtributos(string co, double vl, double c, string ano) {
+    cor = co;
     valor = vl;
     custo_Fixo = c;
     anoFabri = ano;
   }
 
-  
 }
--- main.cs
+++ main.cs
@@ -2,9 +2,6 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    
-    Veiculo veiculo = new Veiculo ("Vermelho", "Ferrari","F40",100.00,"2021",5.000);
-
-    // Concecionaria conc = new Veiculo (veiculo);
+    Console.WriteLine ("Olá"); 
   }
 }
